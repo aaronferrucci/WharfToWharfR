4c465027f0b36ecf6121e8db50e117527127dbd4
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2016-06-19 12:58:41 -0700

    Roxygen2 meta-data.

---
+++ R/w2w2015.R
@@ -0,0 +1,21 @@
+#' Wharf to Wharf race data - 2015
+#'
+#' Data from the 2015 Wharf to Wharf race in Santa Cruz. This dataset is
+#' filtered and cleaned. The code for scraping, filtering and cleaning 
+#' is here:  https://github.com/aaronferrucci/wharf2wharf
+#'
+#' @docType data
+#'
+#' @usage data(w2w2015)
+#'
+#' @format An object of class \code{"datga.frame"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references 
+#'
+#' @source https://github.com/aaronferrucci/wharf2wharf
+#'
+#' @examples
+#' data(w2w2015)
+"w2w2015"
